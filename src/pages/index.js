2799fb4e8bf4781cb4b15098417f4df4300c583f
Riku Vidqvist <rikuvidqvist@hotmail.com>
2020-03-09 12:05:17 +0200

    image added

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+
 const variants = {
   hidden: { opacity: 0 },
   visible: { opacity: 1 },
@@ -21,12 +22,13 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Riku, a web designer." />
     <motion.h1 class="ayylmao"
-        transition={spring} animate={{ scale: 1.1, x: 100, y: 50 }}
+        transition={spring} animate={{ scale: 1.1, x: 100 }}
           >Hello.</motion.h1>
     <motion.p class="ayylmao"
-        transition={spring} animate={{ scale: 1.1, x: 100, y: 50 }}
+        transition={spring} animate={{ scale: 1.1, x: 100 }}
           >Welcome to my site!</motion.p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+    <div style={{ maxWidth: `300px` }}>
+      <Image class="yaboi"/>
     </div>
   </Layout>
 )
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -7,9 +7,8 @@ import SEO from "../components/seo"
 
 const SecondPage = () => (
   <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <SEO title="Portfolio" />
+    <h1>Hi from the portfolio page</h1>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
