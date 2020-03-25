659492b941545d07c79a47b22039aba7f0de47de
Riku Vidqvist <rikuvidqvist@hotmail.com>
2020-03-25 19:38:05 +0200

    again, bruh sound effect #2

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,12 +27,14 @@ const IndexPage = () => (
     <div style={{margingBottom: "2rem"}}>
     <motion.h1 style={{ textAlign: "center", lineHeight: 1.5, fontSize:"5rem", color:"gainsboro" }}
         transition={spring} 
-        animate={{ scale: 1.1, y: 30 }}
+        animate={{ scale: 1, y: 30 }}
           >Hello</motion.h1>
     <motion.p style={{ textAlign: "center", lineHeight: 1.5, fontSize:"3rem", color:"gainsboro"}}
-        animate={{ scale: 1.1, y: 20 }}
+        transition={spring}
+        animate={{ scale: 1, y: 20 }}
           >Welcome to my site!</motion.p>
     </div>
+    <hr/>
     <SpacingGrid/>
     <TextGrid />
     </div>
