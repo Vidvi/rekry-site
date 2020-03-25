df7d9e8a964ee76047b7dd1f7cf5be6576e11406
Riku Vidqvist <rikuvidqvist@hotmail.com>
2020-03-25 16:47:12 +0200

    Transitions work now, for some god forsaken reason

---
+++ src/components/anilink/portraitlink.js
@@ -0,0 +1,63 @@
+import React, { Component} from 'react'
+import TransitionLink from "gatsby-plugin-transition-link"
+import AniLink from "gatsby-plugin-transition-link/AniLink"
+import gsap from 'gsap'
+import { Link } from 'gatsby'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPortrait } from '@fortawesome/free-solid-svg-icons'
+
+const portrait = <FontAwesomeIcon icon={faPortrait} />
+
+class PortraitLink extends Component{
+    constuctor(props) {
+        this.verticalAnimation = this.verticalAnimation.bind(this)
+
+        this.layoutContents = React.createRef()
+        this.transitionCover = React.createRef()
+    }
+
+    verticalAnimation = ({ length }, direction) => {
+        const directionTo = direction === 'up' ? '-100%' : '100%'
+        const directionFrom = direction === 'up' ? '100%' : '-100%'
+    
+        // convert ms to s for gsap
+        const seconds = length
+    
+        return gsap.timeline()
+          .set(this.transitionCover, { y: directionFrom })
+          .to(this.transitionCover, {
+            y: '0%',
+            ease: "power1.easeInOut",
+            duration: seconds / 2,
+          })
+          .set(this.layoutContents, { opacity: 0 })
+          .to(this.transitionCover, {
+            y: directionTo,
+            ease: "power1.easeIn",
+            duration: seconds / 2,
+          })
+      }
+
+      test(entry, node) {
+        return gsap.from(
+          node.querySelectorAll('h2, p, a, pre'),
+          { 
+            opacity: 0, 
+            y: '+=50',
+            duration: 1,
+            stagger: 0.1,
+          },
+        )
+      }
+
+
+      render() {
+          return(
+            <AniLink paintDrip to="/projects#test" hex="#ffffff">
+            { portrait }
+          </AniLink>
+          )
+      }
+
+} export default PortraitLink
--- src/components/grid.js
+++ src/components/grid.js
@@ -1,26 +1,10 @@
 import React, { Component} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-import FormLabel from '@material-ui/core/FormLabel';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import RadioGroup from '@material-ui/core/RadioGroup';
-import Radio from '@material-ui/core/Radio';
-import Paper from '@material-ui/core/Paper';
-import Image3 from '../components/reactimage'
-import Image4 from '../components/gatsbyimage'
-import Image5 from '../components/graphqlimage'
-import TransitionLink from "gatsby-plugin-transition-link"
-import AniLink from "gatsby-plugin-transition-link/AniLink"
-import gsap from 'gsap'
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPortrait } from '@fortawesome/free-solid-svg-icons'
-import { faIdCard } from '@fortawesome/free-solid-svg-icons'
-import { faCode } from '@fortawesome/free-solid-svg-icons'
-
-const portrait = <FontAwesomeIcon icon={faPortrait} />
-const idcard = <FontAwesomeIcon icon={faIdCard} />
-const code = <FontAwesomeIcon icon={faCode} />
+import PortraitLink from "./anilink/portraitlink"
+import PortfolioLink from "./anilink/portfoliolink"
+import ContactLink from "./anilink/contactlink"
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -42,26 +26,9 @@ function SpacingGrid() {
   return (
     <Grid container className={classes.root} spacing={7} justify="center">
 
-      <Grid item xs={4} className={classes.paper}> 
-      <AniLink cover to="../projects" direction="right" color="orange">
-        {portrait}
-      </AniLink>
-      </Grid>
-
-      <Grid item xs={4} className={classes.paper}>
-        <AniLink cover to="../projects" direction="right">
-        <TransitionLink
-          exit={{
-            delay: 0.2
-          }}
-          entry={{
-            delay: 0.2
-          }}>
-              {code}
-        </TransitionLink>
-        </AniLink>
-      </Grid>
-      <Grid item xs={4} className={classes.paper}> {idcard} </Grid>
-      </Grid>
+      <Grid item xs={4} className={classes.paper}> <PortraitLink/> </Grid>
+      <Grid item xs={4} className={classes.paper}> <PortfolioLink/> </Grid>
+      <Grid item xs={4} className={classes.paper}> <ContactLink/> </Grid>
+    </Grid>
   );
 }   export default SpacingGrid
