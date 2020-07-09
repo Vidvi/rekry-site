17174ac1899cd5aca9f38460539c3a290b850eec
Riku Vidqvist <rikuvidqvist@hotmail.com>
2020-07-09 21:53:33 +0300

    bruh

--- src/components/pagegrids/contacttextgrid2.js
+++ src/components/pagegrids/contacttextgrid2.js
@@ -19,8 +19,8 @@ function ContactTextGrid2() {
 
   return (
     <Grid container className={classes.root} spacing={7} justify="center">
-        <Grid item xs={12} md={6} className={classes.paper}> Also this pages github is <a href="https://github.com/Vidvi/rekry-site">here</a> if you want to check it out. You can find this sites source code there!</Grid>
-        <Grid item xs={12} md={6} className={classes.paper}> Well, this is the end of the page. Hope you had a good time! And yes, I like minimalistic design very much. </Grid>
+        <Grid item xs={12} md={6} className={classes.paper}> Fast pages truly are the best! I hope you enjoyed your stay here.</Grid>
+        <Grid item xs={12} md={6} className={classes.paper}> Well, this is also the end of the page. Hope you had a good time! And yes, I like minimalistic design very much. </Grid>
       </Grid>
   );
 }   export default ContactTextGrid2
