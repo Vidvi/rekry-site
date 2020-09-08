cfb6667e64e3450e862d6dbef7581f8899864e60
Riku Vidqvist <rikuvidqvist@hotmail.com>
2020-09-08 12:18:45 +0300

    tab opens instead of redirect

--- src/components/pagegrids/projecttextgrid2.js
+++ src/components/pagegrids/projecttextgrid2.js
@@ -20,7 +20,7 @@ function ProjectTextGrid2() {
 
   return (
     <Grid container className={classes.root} spacing={7} justify="center">
-        <Grid item xs={12} md={6} className={classes.paper}> There's a few projects in my Github. <a link="https://github.com/Vidvi?tab=repositories">View them here.</a> It's always expanding when I'm working on new projects.</Grid>
+        <Grid item xs={12} md={6} className={classes.paper}> There's a few projects in my Github. <a link="https://github.com/Vidvi?tab=repositories" target="_blank">View them here.</a> It's always expanding when I'm working on new projects.</Grid>
         <Grid item xs={12} md={6} className={classes.paper}> I've been also getting into IoT stuff by making my own MagicMirror using Raspberry Pi 3. Automating stuff is always fun and seeing it in action is even better. </Grid>
       </Grid>
   );
